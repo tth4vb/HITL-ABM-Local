8172e96d99a8a1d1e468e7853f41c7840ebf87ce
Trevor Hinkle <trevorhinkle@virginia.edu>
2018-03-13 14:58:13 -0400

    added defector condition to run and server viz

--- run.py
+++ run.py
@@ -4,20 +4,20 @@ model = HITLAdopt(50, 50, 0.8, 5000, 1000, 1000, 10000, 0.05, 0.5)
 for i in range(100):
     model.step()
   
-#print results of output data collector
+'''#print results of output data collector
 results = model.dc_output.get_model_vars_dataframe()
-results.head(25)
+print(results.head(25))
 
 #print impact of HITL
 print ("Total increase in value ($):")
 print(int(results.sum()))
 print("Total increase in value (%):")
 print((int(results.sum())/(2950*2000*100)))
-
+'''
 #print adoption metrics of model output
 results_adoption = model.dc_adoption.get_model_vars_dataframe()
-results_adoption.plot()
-
+print(results_adoption.head(50))
+'''
 #now we'll do a Batch Run, which is where we run several iterations of the model changing certain variables to see impact
 fixed_params = {"height": 50, "width": 50, "density": 0.7, "wus": 1000, "wts": 1000, "wms":1000, "td":10000, "ve": 0}
 variable_params = {"ae": np.arange(0.5, 0.8, 0.01)}
@@ -35,4 +35,4 @@ df.head()
 #create plot of results of batch run
 plt.scatter(df.ae, df.AlgoEffect)
 plt.xlim(0.5, 0.8)
-plt.ylim(0,.2)
+plt.ylim(0,.2)'''
--- server.py
+++ server.py
@@ -23,26 +23,28 @@ def agent_portrayal(agent):
     return portrayal
 
 #creating interactive parameters for user to set in the visualization
-ae_slider = UserSettableParameter('slider', "Starting Algo Effectiveness", 0.6, 0.5, 0.8, 0.01)
-wms_slider = UserSettableParameter('slider', "Weekly Marketing Spend", 2000, 1000, 10000, 100)
-wts_slider = UserSettableParameter('slider', "Weekly Training Spend", 2000, 1000, 10000, 100)
-wus_slider = UserSettableParameter('slider', "Weekly Usability Spend", 2000, 1000, 10000, 100)
-ve_slider = UserSettableParameter('slider', "Visualization effectiveness (in % time saved)", 0.01, 0.01, 0.2, 0.01)
-agent_density_slider = UserSettableParameter('slider', "Starting Algo Effectiveness", 0.5, 0, 1, 0.01)
+ae_slider = UserSettableParameter('slider', "Starting Algo Effectiveness", 0.5, 0.5, 0.8, 0.01)
+wms_slider = UserSettableParameter('slider', "Weekly Marketing Spend", 5000, 1000, 10000, 100)
+wts_slider = UserSettableParameter('slider', "Weekly Training Spend", 1000, 1000, 10000, 100)
+wus_slider = UserSettableParameter('slider', "Weekly Usability Spend", 1000, 1000, 10000, 100)
+ve_slider = UserSettableParameter('slider', "Visualization effectiveness (in % time saved)", 0.05, 0.01, 0.2, 0.01)
+agent_density_slider = UserSettableParameter('slider', "Starting Algo Effectiveness", 0.8, 0, 1, 0.01)
 
 #initialize the grid for the visualization
 grid = CanvasGrid(agent_portrayal, 50, 50, 500, 500)
 
 #create dynamic for the visualization
+chart2 = ChartModule([{"Label":"Avg Output Value Per Person Per Week",
+                     "Color": "blue"}],
+                   data_collector_name='dc_output')
 chart1 = ChartModule([{"Label":"Potential Trialer",
                      "Color": "grey"}, {"Label":"Trialer",
                      "Color": "blue"}, {"Label":"Adopter",
-                     "Color": "red"}],
+                     "Color": "red"}, {"Label":"Defector",
+                     "Color": "green"}],
                    data_collector_name='dc_adoption')
 
-chart2 = ChartModule([{"Label":"Avg Output Differential Value",
-                     "Color": "blue"}],
-                   data_collector_name='dc_output')
+
 
 #create and launch the actual server
 server = ModularServer(HITLAdopt,
