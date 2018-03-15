b2ba69c802b32b70d02a6d4871ce535827eafff3
Trevor Hinkle <trevorhinkle@virginia.edu>
2018-03-15 17:59:35 -0400

    made first test edits on react app

--- hitl_visualization_v1/src/App.js
+++ hitl_visualization_v1/src/App.js
@@ -8,12 +8,22 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Alloy HITL Simulation</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Place holder for eventual visualization
         </p>
+        <p> new paragraph test</p>
+        <TestClass/>
       </div>
+    );
+  }
+}
+
+class TestClass extends Component {
+  render() {
+    return(
+      <div> This is a test </div>
     );
   }
 }
