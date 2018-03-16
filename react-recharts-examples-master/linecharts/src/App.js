b6773b4343bcfa1ff4fa2b26b7323145072880cb
Trevor Hinkle <trevorhinkle@virginia.edu>
2018-03-16 18:52:19 -0400

    added several more example charts to app

--- react-recharts-examples-master/linecharts/src/App.js
+++ react-recharts-examples-master/linecharts/src/App.js
@@ -1827,7 +1827,7 @@ class App extends Component {
   render () {
     return (
       <div>
-      <LineChart width={600} height={400} data={dataFrame}
+      <LineChart width={1400} height={400} data={dataFrame}
             margin={{top: 5, right: 30, left: 20, bottom: 5}}>
        <XAxis dataKey="name"/>
        <YAxis/>
@@ -1839,7 +1839,7 @@ class App extends Component {
        <Line type="monotone" dataKey="Potential Trialer" stroke="#000000" />
       </LineChart>
 
-      <LineChart width={600} height={200} data={dataFrame}
+      <LineChart width={1400} height={200} data={dataFrame}
             margin={{top: 5, right: 30, left: 20, bottom: 5}}>
        <XAxis dataKey="name"/>
        <YAxis/>
@@ -1849,7 +1849,7 @@ class App extends Component {
        <Line type="monotone" dataKey="Defector" stroke="#4286f4" />
        <Line type="monotone" dataKey="Evangelist" stroke="#e5d600" />
       </LineChart>
-      <LineChart width={600} height={200} data={dataFrame}
+      <LineChart width={1400} height={200} data={dataFrame}
             margin={{top: 5, right: 30, left: 20, bottom: 5}}>
        <XAxis dataKey="name"/>
        <YAxis/>
@@ -1858,6 +1858,25 @@ class App extends Component {
        <Legend />
        <Line type="monotone" dataKey="Algo Accuracy Increase" stroke="#4286f4" />
       </LineChart>
+      <LineChart width={1400} height={200} data={dataFrame}
+            margin={{top: 5, right: 30, left: 20, bottom: 5}}>
+       <XAxis dataKey="name"/>
+       <YAxis/>
+       <CartesianGrid strokeDasharray="3 3"/>
+       <Tooltip/>
+       <Legend />
+       <Line type="monotone" dataKey="Algorithm Effect" stroke="#4286f4" />
+      </LineChart>
+      <LineChart width={1400} height={200} data={dataFrame}
+            margin={{top: 5, right: 30, left: 20, bottom: 5}}>
+       <XAxis dataKey="name"/>
+       <YAxis/>
+       <CartesianGrid strokeDasharray="3 3"/>
+       <Tooltip/>
+       <Legend />
+       <Line type="monotone" dataKey="Total Dataset Size" stroke="#4286f4" />
+      </LineChart>
+
       </div>
 
     );
