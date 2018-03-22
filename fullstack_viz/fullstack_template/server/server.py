3b64b42b2aa9799e54503e917f65dac3cecd2943
Trevor Hinkle <trevorhinkle@virginia.edu>
2018-03-22 12:57:02 -0400

    initial commit

--- fullstack_viz/fullstack_template/server/server.py
+++ fullstack_viz/fullstack_template/server/server.py
@@ -1,7 +1,12 @@
 # server.py
 from flask import Flask, render_template
+import random
 
 app = Flask(__name__, static_folder="../static/dist", template_folder="../static")
+
+def get_hello():
+  greeting_list = [‘Ciao’, ‘Hei’, ‘Salut’, ‘Hola’, ‘Hallo’, ‘Hej’]
+  return random.choice(greeting_list)
 
 @app.route("/")
 def index():
@@ -9,7 +14,7 @@ def index():
 
 @app.route("/hello")
 def hello():
-    return 'hello world!'
+    return get_hello()
 
 if __name__ == "__main__":
     app.run()
